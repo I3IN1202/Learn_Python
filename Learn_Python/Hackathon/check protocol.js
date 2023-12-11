d287b0e9a7a7448e48d0432e4537e07232df7838
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-12-11 09:42:20 +0700

    Delete Learn_Python/Hackathon directory

--- Learn_Python/Hackathon/check protocol.js
+++
@@ -1,8 +0,0 @@
-function checkProtocol(url) {
-    const protocol = new URL(url).protocol;
-    if (protocol === 'http:' || protocol === 'https:') {
-        return true;
-    } else {
-        return false;
-    }
-}
