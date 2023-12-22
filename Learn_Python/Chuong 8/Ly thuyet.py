6b29e2c252468cde5d1ee3a458802fa4f880d1de
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2023-12-22 10:03:49 +0700

    Add files via upload

--- Learn_Python/Chuong 8/Ly thuyet.py
+++ Learn_Python/Chuong 8/Ly thuyet.py
@@ -1,3 +1,37 @@
-#x = 'Minh Huy'.lower()
-x = 'do minh huy - huy pro vip'
-print (x.find('huy'))
+#8F
+x = ['Do', 'Minh', 'Huy']
+print(' '.join(x))
+y = 'Do Minh Huy sieu dep trai'
+print(y.split(' '))
+y = [sentence.capitalize() for sentence in x]
+print(y)
+
+#8G - phep toan
+x = 'Minh Huy'
+y = 'Bin'
+z = ['Huy', 'Bin', 'Linh', 'Nam', 'Ngan']
+print(x>y)  #so sanh chuoi: vi tri chu cai dau trong Character map
+print(sorted(z))
+print(sorted(x, reverse=True))
+
+#8H
+x = 'Minh \\Huy'
+print(x)
+
+#8F
+import re
+
+s = 'abcde acbde abbde acccde axyze a123e a@!#e'
+pattern = 'a[\w]{2,3}e'
+# . [] * + ? {} \ | ^ $
+print(re.findall(pattern, s))
+
+#regex tester:
+# + : lap 1 lan tro len
+# * : lap tat ca
+# {}: so lan lap
+# ? : 1 hoac 2
+# \w : [a-zA-Z0-9] >< \W
+#\s : space
+# split cat chuoi
+# findall, split, sub
