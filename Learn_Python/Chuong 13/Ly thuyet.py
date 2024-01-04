a62f088d829f9c2e2e7fff80543b433d6d35db87
Do Minh Huy <112995017+I3IN1202@users.noreply.github.com>
2024-01-04 09:38:42 +0700

    Add files via upload

---
+++ Learn_Python/Chuong 13/Ly thuyet.py
@@ -0,0 +1,11 @@
+#File
+data = open('data.txt', 'w', encoding='utf-8')
+
+# print([line for line in data])
+
+# data.close()
+
+# write
+data.write('Job: Security')
+
+# print(data.read())
---
+++ Learn_Python/Chuong 14/Ly thuyet.py
@@ -0,0 +1,11 @@
+# Module
+from Earth.human import Person as Ps
+
+# print(human.max_age)
+# print(human.bmi(65, 1.7))
+huy = Ps('Mhuy' , 21)
+huy.hello()
+
+# packet
+# Pip
+
